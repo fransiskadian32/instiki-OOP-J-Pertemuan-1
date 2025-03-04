68eb96df66ecd470a35d6199b71463d484eb9061
Dian08082004 <fransiskadian32@gmail.com>
2025-03-04 21:38:47 +0800

    pertemuan 01

---
+++ Pertemuan/src/pertemuan/Pertemuan.java
@@ -0,0 +1,18 @@
+
+ 
+package pertemuan;
+
+/**
+ *
+ * @author Fransiska Dian
+ */
+public class Pertemuan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.print("belajar habasa java");
+    }
+    
+}
